d6bbda214cf70a8102a23483c39e5a054c36ec36
Константин <7528718@gmail.com>
2022-07-27 21:39:21 +0300

    Добавил маршрутизацию в приложение

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,12 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
 import MainScreen from '../../pages/main-screen/main-screen';
+import AddReviewScreen from '../../pages/add-review-screen/add-review-screen';
+import FilmPageScreen from '../../pages/film-page-screen/film-page-screen';
+import MyListScreen from '../../pages/my-list-screen/my-list-screen';
+import PlayerScreen from '../../pages/player-screen/player-screen';
+import SignInScreen from '../../pages/sign-in-screen/sign-in-screen';
+import {AppRoute} from '../../const';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
 
 type AppScreenProps = {
   titleFilm: string;
@@ -9,7 +17,49 @@ type AppScreenProps = {
 
 function App ({titleFilm, genre, releaseDate, posterFilm}: AppScreenProps): JSX.Element {
   return(
-    <MainScreen titleFilm = {titleFilm} genre = {genre} releaseDate = {releaseDate} posterFilm = {posterFilm} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={
+            <MainScreen
+              titleFilm = {titleFilm}
+              genre = {genre}
+              releaseDate = {releaseDate}
+              posterFilm = {posterFilm}
+            />
+          }
+        />
+        <Route
+          path={AppRoute.AddReview}
+          element={<AddReviewScreen />}
+        />
+        <Route
+          path={AppRoute.Film}
+          element={<FilmPageScreen />}
+        />
+        <Route
+          path={AppRoute.MyList}
+          element={<MyListScreen />}
+        />
+        <Route
+          path={AppRoute.Player}
+          element={<PlayerScreen />}
+        />
+        <Route
+          path={AppRoute.SignIn}
+          element={<SignInScreen />}
+        />
+        <Route
+          path={AppRoute.SignIn}
+          element={<SignInScreen />}
+        />
+        <Route
+          path="*"
+          element={<NotFoundScreen />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ project/src/const.ts
@@ -0,0 +1,8 @@
+export enum AppRoute {
+    SignIn = '/login',
+    MyList = '/mylist',
+    Film = '/films/:id',
+    Main = '/',
+    AddReview = '/films/:id/review',
+    Player = '/player/:id'
+  }
