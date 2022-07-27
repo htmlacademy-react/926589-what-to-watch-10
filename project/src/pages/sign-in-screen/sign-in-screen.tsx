c0388ac5dad6a37b4fa8bd46b95bde29a4b0ac17
Константин <7528718@gmail.com>
2022-07-27 21:47:00 +0300

    Добавил компонент Logo и LogoLight

--- project/src/pages/sign-in-screen/sign-in-screen.tsx
+++ project/src/pages/sign-in-screen/sign-in-screen.tsx
@@ -1,13 +1,12 @@
+import Logo from '../../components/logo/logo';
+import LogoLight from '../../components/logo-light/logo-light';
+
 function SignInScreen (): JSX.Element {
   return (
     <div className="user-page">
       <header className="page-header user-page__head">
         <div className="logo">
-          <a href="main.html" className="logo__link">
-            <span className="logo__letter logo__letter--1">W</span>
-            <span className="logo__letter logo__letter--2">T</span>
-            <span className="logo__letter logo__letter--3">W</span>
-          </a>
+          <Logo />
         </div>
 
         <h1 className="page-title user-page__title">Sign in</h1>
@@ -33,11 +32,7 @@ function SignInScreen (): JSX.Element {
 
       <footer className="page-footer">
         <div className="logo">
-          <a href="main.html" className="logo__link logo__link--light">
-            <span className="logo__letter logo__letter--1">W</span>
-            <span className="logo__letter logo__letter--2">T</span>
-            <span className="logo__letter logo__letter--3">W</span>
-          </a>
+          <LogoLight />
         </div>
 
         <div className="copyright">
